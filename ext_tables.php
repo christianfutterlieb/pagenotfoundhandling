40fc7650800c064b6ec009de16d6ba1fdb12ac19
Christian Futterlieb <christian@futterlieb.ch>
2022-11-11 19:19:56 +0100

    [TASK] Remove code for TYPO3 < v10.4

--- ext_tables.php
+++ ext_tables.php
@@ -22,25 +22,14 @@ defined('TYPO3_MODE') or die();
 
     // Add the statistics backend module
     if ($extensionConfiguration->has('enableStatisticsModule') && $extensionConfiguration->get('enableStatisticsModule')) {
-        /** @var \AawTeam\Pagenotfoundhandling\Utility\Typo3VersionUtility $typo3VersionUtility */
-        $typo3VersionUtility = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\AawTeam\Pagenotfoundhandling\Utility\Typo3VersionUtility::class);
-        if ($typo3VersionUtility->isCurrentTypo3VersionAtLeast('10')) {
-            $extensionName = 'Pagenotfoundhandling';
-            $controllerActions= [
-                \AawTeam\Pagenotfoundhandling\Controller\StatisticsController::class => 'index',
-            ];
-        } else {
-            $extensionName = 'AawTeam.Pagenotfoundhandling';
-            $controllerActions= [
-                'Statistics' => 'index',
-            ];
-        }
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            $extensionName,
+            'Pagenotfoundhandling',
             'web',
             'statistics',
             '',
-            $controllerActions,
+            [
+                \AawTeam\Pagenotfoundhandling\Controller\StatisticsController::class => 'index',
+            ],
             [
                 'access' => 'user,group',
                 'iconIdentifier' => 'pagenotfoundhandling-module-statistics',
